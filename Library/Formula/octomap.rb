99677e7906fac4266fef0d3413887f318eaf98c5
Yida Zhang <hitzhangyd@gmail.com>
2013-04-20 19:17:54 -0400

    Use raw patch link in octomap

--- Library/Formula/octomap.rb
+++ Library/Formula/octomap.rb
@@ -11,12 +11,12 @@ class Octomap < Formula
 
   def patches
     [
-      "https://gist.github.com/yida/5427758",
-      "https://gist.github.com/yida/5427759",
-      "https://gist.github.com/yida/5427760",
-      "https://gist.github.com/yida/5427761",
-      "https://gist.github.com/yida/5427762",
-      "https://gist.github.com/yida/5427763"
+      "https://gist.github.com/yida/5427758/raw/16324af3539045c4cfb6081af7772dfe9c05d973/0001-Doxygen-updates-additions.patch",
+      "https://gist.github.com/yida/5427759/raw/8a3ff0578407904506930f968c66797df47bd5d8/0002-Fixed-compilation-with-OSX-10.8-std-isnan-Issue-23.patch",
+      "https://gist.github.com/yida/5427760/raw/0bbdb34c87e446f11deb86c24c1d330179f0f28c/0003-Fix-.dylib-in-CMake-configs-for-Mac-OS-Issue-23.patch",
+      "https://gist.github.com/yida/5427761/raw/e1bcd3f31e0b5c96fe9fd3e67b9f232836115438/0004-Modify-CMake-system-to-support-Mac-OSX-using-Homebre.patch",
+      "https://gist.github.com/yida/5427762/raw/cf42c12747111d66cd17c8ec82d5a578bd07544c/0005-Fix-cmake-file-to-match-homebrew-install.patch",
+      "https://gist.github.com/yida/5427763/raw/f575d8173124c8a27a3aea39ed7cc5b886a37c41/0006-clean-debugging-message.patch"
     ]
   end
 
